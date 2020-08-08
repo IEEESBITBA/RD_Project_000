09a040093ea53c772fbe4b05659ff2f89f7c9ea1
Alan <31071359+amechu@users.noreply.github.com>
2020-08-08 20:26:34 -0300

    Update main

--- Main.py
+++ Main.py
@@ -1,15 +1,26 @@
 import cv2
 
-capture = cv2.VideoCapture(0)
+capture = cv2.VideoCapture(0) #Tomo como device de captura la camara web
 
 while (True):
 
-    ret, frame = capture.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    ret, frame = capture.read() #Leo un frame (de esta manera lea tan rapido como pueda no respeta fps)
 
-    cv2.imshow('video', gray)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  #Cambio el frame a grayscale
 
-    if cv2.waitKey(1) == 27:
+    #for i in frame.shape()[1]:
+    #    for j in frame.shape()[0]:
+    #        sum += frame[i, j]
+
+    #gray = cv2.putText(gray, str(sum), (int(frame.shape[1] / 2), int(frame.shape[0] / 2)), cv2.FONT_HERSHEY_COMPLEX, 1,
+                      255)  # Pongo algun texto
+    gray = cv2.putText(gray, "Hola", (int(frame.shape[1]/2), int(frame.shape[0]/2)), cv2.FONT_HERSHEY_COMPLEX, 1, 255) #Pongo algun texto
+
+    cv2.imshow('gris', gray) #Muestro imagen en escala de grises
+    cv2.imshow('original', frame) #Muestro imagen original
+
+
+    if cv2.waitKey(1) == 27: #Espero la tecla para salir
         break
 
 capture.release()
